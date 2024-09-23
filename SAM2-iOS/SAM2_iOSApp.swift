b03c00e15dbe19e51e8d84834da6c2d8ecd90e1c
Alex Haugland <alex.haugland@gmail.com>
2024-09-23 15:38:20 -0400

    ios skeleton

---
+++ SAM2-iOS/SAM2_iOSApp.swift
@@ -0,0 +1,12 @@
+//
+
+import SwiftUI
+
+@main
+struct SAM2_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SAM2-iOSTests/SAM2_iOSTests.swift
@@ -0,0 +1,12 @@
+//
+
+import Testing
+@testable import SAM2_iOS
+
+struct SAM2_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
